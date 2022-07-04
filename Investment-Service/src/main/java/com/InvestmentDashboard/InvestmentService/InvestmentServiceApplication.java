807d28c7495c4d3eeeca6a15fdce0c6aa66d5142
Ananou Kevin <107629615+Venenium@users.noreply.github.com>
2022-07-04 14:59:50 +0200

    Added API-GATEWAY and REGISTRY-SERVICE

--- Investment-Service/src/main/java/com/InvestmentDashboard/InvestmentService/InvestmentServiceApplication.java
+++ Investment-Service/src/main/java/com/InvestmentDashboard/InvestmentService/InvestmentServiceApplication.java
@@ -3,8 +3,10 @@ package com.InvestmentDashboard.InvestmentService;
 import com.InvestmentDashboard.InvestmentService.Controller.InvestmentController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InvestmentServiceApplication {
 
 	public static void main(String[] args) {
