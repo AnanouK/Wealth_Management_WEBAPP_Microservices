6a7a88c53f3477d7398aa362a1ae0de05ad0969e
Ananou Kevin <107629615+Venenium@users.noreply.github.com>
2022-07-05 18:41:25 +0200

    Update

--- Investment-Service/src/main/java/com/InvestmentDashboard/InvestmentService/InvestmentServiceApplication.java
+++ Investment-Service/src/main/java/com/InvestmentDashboard/InvestmentService/InvestmentServiceApplication.java
@@ -3,10 +3,8 @@ package com.InvestmentDashboard.InvestmentService;
 import com.InvestmentDashboard.InvestmentService.Controller.InvestmentController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class InvestmentServiceApplication {
 
 	public static void main(String[] args) {
