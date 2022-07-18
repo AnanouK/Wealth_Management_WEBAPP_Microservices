dc53f6f5ca8eb2bf1bba86169b173457fbcf165d
Ananou Kevin <107629615+Venenium@users.noreply.github.com>
2022-07-18 21:41:30 +0200

    Bugs fix

--- Investment-Service/src/main/java/com/InvestmentDashboard/InvestmentService/Controller/InvestmentController2.java
+++ Investment-Service/src/main/java/com/InvestmentDashboard/InvestmentService/Controller/InvestmentController2.java
@@ -2,17 +2,14 @@ package com.InvestmentDashboard.InvestmentService.Controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 @ResponseStatus(HttpStatus.OK)
 @AllArgsConstructor
 public class InvestmentController2 {
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     @ResponseStatus(code = HttpStatus.OK, reason = "OK")
     public String ok() {
--- User-Service/src/main/java/com/InvestmentDashboard/UserService/Controller/UserController.java
+++ User-Service/src/main/java/com/InvestmentDashboard/UserService/Controller/UserController.java
@@ -5,7 +5,7 @@ import com.InvestmentDashboard.UserService.Service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController
 {
     private final UserService userService;
@@ -14,13 +14,13 @@ public class UserController
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @PostMapping("/adduser")
+    @CrossOrigin(origins = "*")
+    @PostMapping("/add")
     public String adduser (@RequestBody User user)
     {
         return userService.adduser(user);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String login(@RequestParam String username, @RequestParam String password)
     {
