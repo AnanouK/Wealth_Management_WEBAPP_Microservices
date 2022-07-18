73a0df8258c2ce2095a236f67a4c26be46ac0ff2
Ananou Kevin <107629615+Venenium@users.noreply.github.com>
2022-07-18 19:30:07 +0200

    Added / http 200 for USER SERVICE for Ingress

---
+++ User-Service/src/main/java/com/InvestmentDashboard/UserService/Controller/UserController2.java
@@ -0,0 +1,21 @@
+package com.InvestmentDashboard.UserService.Controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@ResponseStatus(HttpStatus.OK)
+@AllArgsConstructor
+public class UserController2 {
+
+    @GetMapping("/")
+    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
+    public String ok() {
+        return "Class Level HTTP Status Overriden. The HTTP Status will be OK (CODE 200)\n";
+    }
+}
