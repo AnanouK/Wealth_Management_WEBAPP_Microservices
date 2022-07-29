4832ac4760011d5069bfa6333b8e9abe87a29463
Ananou Kevin <107629615+Venenium@users.noreply.github.com>
2022-07-29 21:33:42 +0200

    Bug Fix (Multiples rows for the same date)

--- Statistics-Service/src/main/java/com/venenium/StatisticsService/Service/StatisticsService.java
+++ Statistics-Service/src/main/java/com/venenium/StatisticsService/Service/StatisticsService.java
@@ -3,7 +3,10 @@ import com.venenium.StatisticsService.Model.Statistics;
 import com.venenium.StatisticsService.Repository.StatisticsRepository;
 import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,14 +48,46 @@ public class StatisticsService {
 
     }
 
-    public Statistics addStatistic(Statistics statistics) {
-        Statistics newstats = new Statistics();
-        newstats.setName(statistics.getName());
-        newstats.setStart(statistics.getStart());
-        newstats.setActual(statistics.getActual());
-        newstats.setUsername(statistics.getUsername());
+    public ResponseEntity<String> addStatistic(Statistics statistics) {
 
-        return statisticsRepository.save(newstats);
+         List<Statistics> getData = statisticsRepository.findByNameAndUsername(statistics.getName(),statistics.getUsername());
+
+         //If the row already exist with the same date, we will change it and replace it instead of create a new one
+         if (getData.size()!=0)
+         {
+             if (getData.get(getData.size()-1).getStart().equals(statistics.getStart()))
+             {
+                 Statistics lastStat = getData.get(getData.size()-1);
+                 lastStat.setId(getData.get(getData.size()-1).getId());
+                 lastStat.setActual(statistics.getActual());
+                 lastStat.setCapital(statistics.getCapital());
+                 statisticsRepository.save(lastStat);
+                 return new ResponseEntity<String>(HttpStatus.OK);
+            }
+
+             else
+             {
+                 Statistics newstats = new Statistics();
+                 newstats.setName(statistics.getName());
+                 newstats.setStart(statistics.getStart());
+                 newstats.setActual(statistics.getActual());
+                 newstats.setUsername(statistics.getUsername());
+                 statisticsRepository.save(newstats);
+                 return new ResponseEntity<String>(HttpStatus.CREATED);
+             }
+         }
+
+         // else we create the new statistics
+         else
+         {
+             Statistics newstats = new Statistics();
+             newstats.setName(statistics.getName());
+             newstats.setStart(statistics.getStart());
+             newstats.setActual(statistics.getActual());
+             newstats.setUsername(statistics.getUsername());
+             statisticsRepository.save(newstats);
+             return new ResponseEntity<String>(HttpStatus.CREATED);
+         }
     }
 
     public List<Statistics> allStatistics() {
@@ -60,9 +95,10 @@ public class StatisticsService {
         return statisticsRepository.findAll();
     }
 
-    public void delete(String name) {
+    @Transactional
+    public void delete(String name, String username) {
 
-        statisticsRepository.deleteAllByName(name);
+        statisticsRepository.deleteByNameAndUsername(name,username);
     }
 
     public void getDataDonut(ArrayList<Statistics> investments) {
@@ -73,5 +109,13 @@ public class StatisticsService {
         }
         System.out.println(investments);
 
+    }
+    @Transactional
+    public void deleteStatistic(int id) {
+        statisticsRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void deleteAll(String username) { statisticsRepository.deleteByUsername(username);
     }
 }
